38fe434c3566688e6f8d0900711bce74e01e09a2
GBTC408014UR <GBTC408014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-14 16:40:30 -0400

    HTML Fragments

---
+++ src/main/java/com/example/demo/Springboot16Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16Application.class, args);
+	}
+
+}
